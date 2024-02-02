a0dba7677ff7d710cd081d4e2f6e00d28fa4147a
bryanathll <bryannatanael77@gmail.com>
2024-02-02 16:32:23 +0700

    add flask app

--- app.py
+++ app.py
@@ -7,5 +7,9 @@ app = Flask(__name__)
 def main():
     return "<h1>Hello world!!</h1>"
 
+@app.route('/<name>')
+def user(name):
+    return f'<h1>Hello {name}</h1>'
+
 if __name__ == '__main__':
     app.run()
